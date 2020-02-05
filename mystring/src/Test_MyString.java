903f8ec5bfa6351aac19ea4d338137c31e36bba5
PaulBuilds <paulbuildslab@gmail.com>
2020-02-05 08:40:23 +0100

    Updated MyString | School Project Nr.3
    Added Test Class,
    Updated imports

--- mystring/src/Test_MyString.java
+++ mystring/src/Test_MyString.java
@@ -3,9 +3,22 @@ public class Test_MyString {
     public static void main(String[] args) {
 
         MyString ms1,ms2,ms3,ms4;
+        //String 1
         ms1 = new MyString();
         char [] t = {'H','a','l','l','o'};
         ms1.setText(t);
+        System.out.println(ms1.getText()+","+ms1.getLth());
 
+        //String 2
+        ms2 = new MyString();
+        chat [] s = {'M','i','t','t','w','o','c','h'};
+        ms2.setText(s);
+
+        //CharAt
+
+        System.out.println("1. Zeichen: " + ms1.charAt(0));
+        System.out.println("3. Zeichen: " + ms1.charAt(3));
+        System.out.println("letztes Zeichen: " + ms1.charAt(ms1.getLth() - 1));
+        System.out.println("Verboten: " + ms1.charAt(ms1.getLth());
         }
 }
