e827623bccd3d1ae5a99243b299f3f84589106f9
Paul-Lukas Huber <paullukas.huber@gmail.com>
2021-03-09 18:50:13 +0100

    Started School Prject Nr. 5 | Algorithms
    At the moment, we are working on binary search after looking at networking. This is a repetition of the topics so in itself nothing new, however we are programming this independently for the first time. However, I am aware that Java has implemented this by default.

---
+++ algorithms/src/BinSearch.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class BinSearch {
+
+    public static void main(String[] args) {
+
+
+        //Input
+        Scanner input = new Scanner(System.in);
+
+        //Array
+        int feld[] = new int[10];
+
+        //Array Input
+        System.out.println("Geben Sie die Listenelemente Sortiert ein!");
+        System.out.println("Eingabe: ");
+        for (int i = 0; i < feld.length; i++) {
+            feld[i] = input.nextInt();
+        }
+
+        //Input Search
+        System.out.println("Geben Sie nun die gesuchte Zahl an.");
+        System.out.println("Eingabe: ");
+        int x = input.nextInt();
+
+        //Declaration
+        int m = -1;
+        int links;
+        int rechts;
+
+        links = 0;
+        rechts = feld.length - 1;
+
+        boolean s = false;
+
+        while (links <= rechts) {
+            m = links + rechts / 2;
+            if (feld[m] == x) {
+                System.out.println(m);
+                s = true;
+                break;
+            }
+            if (x < feld[m]) {
+                rechts = m - 1;
+            }
+            if (x > feld[m]) {
+                links = m + 1;
+            }
+        }
+        if (s = false) {
+            System.out.println(-1);
+        }
+    }
+}
